c79c8cd63de174b74dbe952b473df0fed787d553
asnganje <abd.nganje@gmail.com>
2024-08-05 14:59:43 +0300

    Update styling

--- src/Components/NewsCentre/NewsCentre.jsx
+++ src/Components/NewsCentre/NewsCentre.jsx
@@ -1,23 +1,23 @@
 import './NewsCentre.css'
-import { announcementsInfo, postsInfo } from './newsData.jsx'
+import { announcementsInfo, documentsInfo, postsInfo } from './newsData.jsx'
 const NewsCentre = () => {
   return (
     <section className='news-container'>
       <div>
           <hr className='news-hr'/>
-          <h4>Recent Posts</h4>
+          <h4 className='main-head'>Recent Posts</h4>
           <div className='posts-main-div'>
               {
                 postsInfo.map((post)=> {
-                  const {id, image, title, date, icon } = post
+                  const {id, image, title, date, link, icon } = post
                   return (
                     <article key={id} className='posts-article'>
                         <img src={image} alt="title" className='post-img' />
-                        <div className="">
-                          <p className='post-title'>{title}</p>
+                        <div>
+                          <a href={link} className='post-title'>{title}</a>
                           <p className='post-timing'>
                             {icon}
-                            Posted on: <span>{date}</span>
+                            Posted on: <span className='date'>{date}</span>
                           </p>
                         </div>
                     </article>
@@ -28,14 +28,30 @@ const NewsCentre = () => {
       </div>
       <div className='announcements-container'>
           <hr className='news-hr2'/>
-          <h4>Recent Announcements</h4>
+          <h4 className='main-head'>Recent Announcements</h4>
           <div className="announcements-main-div">
               {
                 announcementsInfo.map((announce)=> {
                   const {id, image, title, icon, pdf, date} = announce
                   return(
-                    <article key={id}>
-                      
+                    <article key={id} className='announcements-article'>
+                      <img src={image} alt={title} className='announcement-img' />
+                      <div>
+                        <p className='announcement-title'>{title}</p>
+                        <div className='announcement-info'>
+                          <p className='icon-and-date'>
+                            {icon}
+                            {date}
+                          </p>
+                          <span className="separator"></span>
+                          <p className='pdf-link'>
+                            {pdf}
+                            <a href="#">
+                              Download
+                            </a>
+                          </p>
+                        </div>
+                      </div>
                     </article>
                   )
                 })
@@ -43,8 +59,21 @@ const NewsCentre = () => {
           </div>
       </div>
       <div>
-      <hr className='news-hr3'/>
-        <h4>Document Center</h4>
+          <hr className='news-hr3'/>
+          <h4 className='main-head'>Document Center</h4>
+          <div className="docs-main-div">
+            {
+              documentsInfo.map((doc)=> {
+                const {id, title, pdf, link} = doc
+                return(
+                  <article key={id} className='docs-article'>
+                    {pdf}
+                    <a href={link} className='document-link'>{title}</a>
+                  </article>
+                )
+              })
+            }
+          </div>
       </div>
     </section>
   )
--- src/Components/NewsCentre/newsData.jsx
+++ src/Components/NewsCentre/newsData.jsx
@@ -5,27 +5,31 @@ import news from '../../assets/new.gif'
 import { Calendar } from 'lucide-react'
 import { Clock9 } from 'lucide-react'
 import { FaFilePdf } from "react-icons/fa";
+import { FaRegFilePdf } from "react-icons/fa";
 
 export const postsInfo = [
   {
     id: 1,
     image: prog1,
+    link: '#',
     title: 'Muumini asoma biblia',
-    icon:<Calendar />,
+    icon:<Calendar className='calendar' />,
     date: '31 May, 2024'
   },
   {
     id: 2,
     image: prog2,
+    link: '#',
     title: 'Waumini katika maombi',
-    icon:<Calendar />,
+    icon:<Calendar className='calendar' />,
     date: '02 June, 2024'
   },
   {
     id: 3,
     image: prog3,
+    link: '#',
     title: 'Believer in deep prayers',
-    icon:<Calendar />,
+    icon:<Calendar className='calendar' />,
     date: '11 July, 2024'
   },
 ]
@@ -35,7 +39,7 @@ export const announcementsInfo = [
     id: 1,
     image: news,
     title: 'Joining instructions',
-    icon:<Clock9 />,
+    icon:<Clock9 className='clock'/>,
     pdf: <FaFilePdf />,
     date: 'June 14, 2024'
   },
@@ -43,7 +47,7 @@ export const announcementsInfo = [
     id: 2,
     image: news,
     title: 'Services offered',
-    icon:<Clock9 />,
+    icon:<Clock9 className='clock' />,
     pdf: <FaFilePdf />,
     date: 'May 2, 2024'
   },
@@ -51,8 +55,29 @@ export const announcementsInfo = [
     id: 3,
     image: news,
     title: 'Programs bronchure',
-    icon:<Clock9 />,
+    icon:<Clock9 className='clock' />,
     pdf: <FaFilePdf />,
     date: '11 July, 2024'
+  },
+]
+
+export const documentsInfo = [
+  {
+    id: 1,
+    title: 'QUALIFICATIONS FOR THE CENTRE PROGRAMS',
+    pdf: <FaRegFilePdf className='docsPdf' />,
+    link: '#'
+  },
+  {
+    id: 2,
+    title: 'REPORT OF THE CENTRE ACTIVITIES OF THE YEAR',
+    pdf: <FaRegFilePdf className='docsPdf'/>,
+    link: '#'
+  },
+  {
+    id: 3,
+    title: 'FOMU YA KUOMBA KUJIUNGA NA TAASISI HII',
+    pdf: <FaRegFilePdf className='docsPdf'/>,
+    link: '#'
   },
 ]
